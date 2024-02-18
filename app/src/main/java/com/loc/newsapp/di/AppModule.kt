f5c43cebcae0ff0668810217ba3a7d6fb7054304
YU-HASUNG <dbgktjd@naver.com>
2024-02-18 23:29:00 +0900

    [240218] Dagger Hilt 기본설정

---
+++ app/src/main/java/com/loc/newsapp/di/AppModule.kt
@@ -0,0 +1,34 @@
+package com.loc.newsapp.di
+
+import android.app.Application
+import com.loc.newsapp.data.manager.LocalUserMangerImpl
+import com.loc.newsapp.domain.manager.LocalUserManger
+import com.loc.newsapp.domain.usecases.AppEntryUseCases
+import com.loc.newsapp.domain.usecases.ReadAppEntry
+import com.loc.newsapp.domain.usecases.SaveAppEntry
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideLocalUserManager(
+        application: Application
+    ): LocalUserManger = LocalUserMangerImpl(application)
+
+    @Provides
+    @Singleton
+    fun provideAppEntryUseCases(
+        localUserManger: LocalUserManger
+    ) = AppEntryUseCases(
+        readAppEntry = ReadAppEntry(localUserManger),
+        saveAppEntry = SaveAppEntry(localUserManger)
+    )
+
+}
